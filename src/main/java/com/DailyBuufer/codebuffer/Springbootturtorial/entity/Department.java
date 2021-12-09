e0311309399ff5919da91f9231efc94cf5bc453c
palugunak <palugunak@gmail.com>
2021-12-09 14:39:54 -0500

    added lombok

--- src/main/java/com/DailyBuufer/codebuffer/Springbootturtorial/entity/Department.java
+++ src/main/java/com/DailyBuufer/codebuffer/Springbootturtorial/entity/Department.java
@@ -1,5 +1,10 @@
 package com.DailyBuufer.codebuffer.Springbootturtorial.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -7,7 +12,10 @@ import javax.persistence.Id;
 import javax.validation.constraints.NotBlank;
 
 @Entity
-
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Department {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
@@ -17,56 +25,5 @@ public class Department {
     private String deparmentAddress;
     private String deparmentCode;
 
-    public Department(Long deparmentId, String deparmentName, String deparmentAddress, String deparmentCode) {
-        this.deparmentId = deparmentId;
-        this.deparmentName = deparmentName;
-        this.deparmentAddress = deparmentAddress;
-        this.deparmentCode = deparmentCode;
-    }
 
-    @Override
-    public String toString() {
-        return "Deparment{" +
-                "deparmentId=" + deparmentId +
-                ", deparmentName='" + deparmentName + '\'' +
-                ", deparmentAddress='" + deparmentAddress + '\'' +
-                ", deparmentCode='" + deparmentCode + '\'' +
-                '}';
-    }
-
-    public Department(){
-
-    }
-
-    public Long getDeparmentId() {
-        return deparmentId;
-    }
-
-    public void setDeparmentId(Long deparmentId) {
-        this.deparmentId = deparmentId;
-    }
-
-    public String getDeparmentName() {
-        return deparmentName;
-    }
-
-    public void setDeparmentName(String deparmentName) {
-        this.deparmentName = deparmentName;
-    }
-
-    public String getDeparmentAddress() {
-        return deparmentAddress;
-    }
-
-    public void setDeparmentAddress(String deparmentAddress) {
-        this.deparmentAddress = deparmentAddress;
-    }
-
-    public String getDeparmentCode() {
-        return deparmentCode;
-    }
-
-    public void setDeparmentCode(String deparmentCode) {
-        this.deparmentCode = deparmentCode;
-    }
 }
